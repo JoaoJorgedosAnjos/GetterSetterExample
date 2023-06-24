491e1ac31b599a1ab4bc7d296af447ea042bd692
joaojorge <joaojorgedosanjos@gmail.com>
2023-06-24 20:00:47 -0300

    correção

--- src/escola/Escola.java
+++ src/escola/Escola.java
@@ -1,2 +1,16 @@
-package escola;public class Escola {
+package escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        Aluno felipe = new Aluno();
+
+        felipe.setNome("Felipe");
+        felipe.setIdade(8);
+        felipe.setSexo("Masculino");
+
+        System.out.println("O aluno "+ felipe.getNome() + " tem " + felipe.getIdade()+ " anos");
+        System.out.println(felipe.getSexo());
+        //RESULTADO NO CONSOLE
+        //O aluno Felipe tem 8 anos
+    }
 }
